6df28bdc73134a402c0daf74e30d2fca2aa362e2
Eduardo <eduardodiaz250196@gmail.com>
2021-11-16 23:19:16 -0300

    signup basic style

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -4,7 +4,16 @@ import './signup.css'
 const Signup = () => {
   return (
     <>
-      <h2>Sign Up</h2>
+      <div className="df-jc-ac-fdc">
+      <h2 >Sign Up</h2>
+      <form  className="form df-jc-ac-fdc">
+            <input  name="name" className="input" id="task-title" type="text" placeholder="name" />
+            <input  name="name" className="input" id="task-title" type="text" placeholder="surname" />
+            <input  name="name" className="input" id="task-title" type="email" placeholder="email" />
+            <input  name="email" className="input" id="task-responsible" type="password" placeholder="password" />
+            <button className="btn" type="sumbit">Log in</button>
+      </form>
+      </div>
     </>
   )
 }
